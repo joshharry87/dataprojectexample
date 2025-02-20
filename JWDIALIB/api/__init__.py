9d491d67f4c95b3df4dbe469ec0a5b1209f4b328
Josh Harry <josh.c.harry@outlook.com>
2025-02-20 10:47:09 +0800

    setting up dev environment - analyst/function app

--- JWDIALIB/api/__init__.py
+++ JWDIALIB/api/__init__.py
@@ -1,3 +1,30 @@
-    """
+"""
         Generic function logic for accessing APIs
+"""
+    
+import requests as req
+import bs4
+import pandas as pd
+
+
+
+def get_dataframe_from_url(url: str, data: str):
+    """Gets dataframe from the target source.
+
+    Args:
+        url (str): API URL
     """
+    
+    if len(data) > 0:
+        response = req.get(url, data=data)
+    else:
+        response = req.get(url)
+    
+    # will need to run this and see what changes need to be made to the response 
+    #  to generalise
+    df = pd.DataFrame(response.content)
+    
+    return df
+
+
+    
--- JWDIALIB/api/targetapi.py
+++ JWDIALIB/api/targetapi.py
@@ -1,2 +1,14 @@
-    """Specific functions for target data
+"""Specific functions for target data
+"""
+import api
+
+
+def get_cpi_data():
+    """Returns CPI Data as a dataframe
+
+    Returns:
+        Dataframe: Dataframe of the ABS CPI data
     """
+    data = api.get_dataframe_from_url("")
+    return data
+    
