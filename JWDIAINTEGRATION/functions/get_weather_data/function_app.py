da76f1b0f9e068ebb0e45bd509feee667a16e55b
Josh Harry <josh.c.harry@outlook.com>
2025-02-25 15:27:26 +0800

    WiP

---
+++ JWDIAINTEGRATION/functions/get_weather_data/function_app.py
@@ -0,0 +1,22 @@
+import logging
+import azure.functions as func
+import requests
+
+app = func.FunctionApp()
+
+@app.timer_trigger(schedule="0 * * * * *", arg_name="myTimer", run_on_startup=False,
+              use_monitor=False) 
+def timer_trigger(myTimer: func.TimerRequest) -> None:
+    if myTimer.past_due:
+        logging.info('The timer is past due!')
+
+    logging.info('Python timer trigger function executed.')
+
+@app.timer_trigger(schedule="", arg_name="myTimer", run_on_startup=False,
+              use_monitor=False) 
+def get_weather_data(myTimer: func.TimerRequest) -> None:
+    
+    if myTimer.past_due:
+        logging.info('The timer is past due!')
+
+    logging.info('Python timer trigger function executed.')
