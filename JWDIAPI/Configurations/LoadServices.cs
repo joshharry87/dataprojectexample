7b97245448979fb71059aea1d867a62b09c5f2f4
Josh Harry <josh.c.harry@outlook.com>
2025-03-19 12:03:14 +0800

    added scalar

--- JWDIAPI/Configurations/LoadServices.cs
+++ JWDIAPI/Configurations/LoadServices.cs
@@ -1,10 +1,9 @@
-
 using JWDIACONTRACTS.Services.Weather;
 using JWDIACONTRACTS.Interfaces.Weather;
 using JWDIACONTRACTS.Interfaces.GeoSurvey;
 using JWDIACONTRACTS.Services.GeoSurvey;
 
-
+// this should self generate reading by assembly and naming conventions
 namespace JWDIAPI.Configurations
 {
     public static class LoadServices
--- JWDIAPI/Program.cs
+++ JWDIAPI/Program.cs
@@ -3,6 +3,7 @@ using JWDIADATA.Data;
 using JWDIAPI.Configurations;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Scalar.AspNetCore;
 
 // Need to add a bunch of services 
 
@@ -40,6 +41,7 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
     app.MapOpenApi();
+    app.MapScalarApiReference();
 }
 
 
